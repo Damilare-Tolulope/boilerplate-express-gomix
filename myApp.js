09bcfa639f408fd3ee78d9ae709cdf12b0818465
Damilare <54348429+Damilare-Tolulope@users.noreply.github.com>
2021-09-08 16:14:56 +0100

    Using env files to pass variables

--- myApp.js
+++ myApp.js
@@ -1,7 +1,15 @@
-var express = require('express');
+var express = require("express")
 var app = express();
 
-
+app.get("/json", function(req, res){
+  if(process.env.MESSAGE_STYLE==="uppercase"){
+    console.log("hi")
+    res.json({"message": "HELLO JSON"})
+  }
+  else{
+    res.json({"message": "Hello json"})
+  }
+});
 
 
 
